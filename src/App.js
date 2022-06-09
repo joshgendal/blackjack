ec6d693f78f33404af75416c6f4351f7fc441e02
joshgendal <68124800+joshgendal@users.noreply.github.com>
2022-06-09 00:41:08 -0400

    deal initial hand a display simple text as card to user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import BeginHandForm from "./components/BeginHandForm";
 import { useState } from "react";
@@ -7,6 +6,8 @@ import Game from "./components/Game";
 function App() {
   const [numPlayers, setNumPlayers] = useState();
   const [handHasStarted, setHandHasStarted] = useState(false);
+  // if hand hasn't started (user has not selected amount of players),
+  // render form to select amount of players as well as button to submit
   if (!handHasStarted) {
     return (
       <div className="App">
@@ -17,6 +18,7 @@ function App() {
       </div>
     );
   } else {
+    // if hand has started, render Game
     return <Game numPlayers={numPlayers} />;
   }
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,46 +1,50 @@
 import { useReducer, useEffect } from "react";
 import { dealCard } from "../utils/game";
+
 const intitialState = {
   hands: [],
 };
 
 const reducer = (state, { type, payload }) => {
   switch (type) {
-    case "SET_INITIAL_HANDS":
+    case "ADD_INITIAL_HAND":
       return {
-        hands: payload,
+        hands: [...state.hands, payload],
       };
     default:
       return state;
   }
 };
 
-const dealInitialHand = () => {
-  // return new Promise((resolve, reject) => {
-  const cardOne = dealCard();
-  const cardTwo = dealCard();
-  return [cardOne, cardTwo];
-  // });
-};
+const dealInitialHand = () => [dealCard(), dealCard()];
 
 const Game = (props) => {
   const { numPlayers } = props;
 
-  const dealPlayers = () => {
-    console.log("deal pl called");
-    let hands = [];
-    for (var i = 0; i < numPlayers; i++) {
-      const newHand = dealInitialHand();
-      hands.push(newHand);
-    }
-    dispatch({ type: "SET_INITIAL_HANDS", payload: hands });
-  };
   useEffect(() => {
-    // console.log("effect");
+    const dealPlayers = () => {
+      for (let i = 0; i < numPlayers; i++) {
+        const newHand = dealInitialHand();
+        dispatch({ type: "ADD_INITIAL_HAND", payload: newHand });
+      }
+    };
+    console.log("effect");
     return () => dealPlayers();
-  }, []);
+  }, [numPlayers]);
   const [state, dispatch] = useReducer(reducer, intitialState);
-  return <div>GAME</div>;
+  return (
+    <>
+      {state.hands.map((hand, outerIndex) => {
+        return (
+          <div key={outerIndex}>
+            {hand.map((card, innerIndex) => (
+              <span key={innerIndex}>{card}</span>
+            ))}
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Game;
